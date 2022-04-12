f5ac3040cf23f802cd65fd975832e581ef059186
BhavyaAgg004 <bhavya.a.aggarwal@capgemini.com>
2022-04-12 11:46:10 +0530

    Update Vendor.java

--- src/main/java/com/cg/entity/Vendor.java
+++ src/main/java/com/cg/entity/Vendor.java
@@ -1,5 +1,66 @@
 package com.cg.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Vendor")
 public class Vendor {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int vendorId;
+	
+	@Column(name="Vendor Name")
+	private String vendorName;
+	
+	@Column(name="Vendor Contact")
+	private int vendorContact;
+	
+	public Vendor() {
+		super();
+	}
+
+	public Vendor(int vendorId, String vendorName, int vendorContact) {
+		super();
+		this.vendorId = vendorId;
+		this.vendorName = vendorName;
+		this.vendorContact = vendorContact;
+	}
+
+	public int getVendorId() {
+		return vendorId;
+	}
+
+	public void setVendorId(int vendorId) {
+		this.vendorId = vendorId;
+	}
+
+	public String getVendorName() {
+		return vendorName;
+	}
+
+	public void setVendorName(String vendorName) {
+		this.vendorName = vendorName;
+	}
+
+	public int getVendorContact() {
+		return vendorContact;
+	}
+
+	public void setVendorContact(int vendorContact) {
+		this.vendorContact = vendorContact;
+	}
+
+	@Override
+	public String toString() {
+		return "Vendor [vendorId=" + vendorId + ", vendorName=" + vendorName + ", vendorContact=" + vendorContact + "]";
+	}
+	
+	
 
 }

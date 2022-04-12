d90b8a98284ab1a81c3bbd32f86e9f5bc9f61d99
KaustubhC <kaustubhchillure27@gmail.com>
2022-04-12 12:00:31 +0530

    changes 7

--- src/main/java/com/cg/Service/CustomerService.java
+++ src/main/java/com/cg/Service/CustomerService.java
@@ -1,11 +1,16 @@
-package com.cg.Service;
+package com.cg.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cg.entity.Customer;
+import com.cg.repository.CustomerRepository;
+
 @Service
 public class CustomerService {
   @Autowired
-  private CustomerService cservice;
+  private CustomerRepository rrepo;
+  
+  
   
 }

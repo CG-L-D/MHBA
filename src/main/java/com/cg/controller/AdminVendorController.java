cf7bf8bc42c2d858386ab9ad2f99dcabacfda90f
BhavyaAgg004 <bhavya.a.aggarwal@capgemini.com>
2022-04-17 15:57:55 +0530

    annotation
    basic

--- src/main/java/com/cg/controller/AdminVendorController.java
+++ src/main/java/com/cg/controller/AdminVendorController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.entity.Vendor;
 import com.cg.service.VendorService;
 
+@RestController
 public class AdminVendorController {
 	
 	@Autowired
